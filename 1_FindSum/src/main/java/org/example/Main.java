9207b09abf05f339ed18575e47b79ea24f944ef5
Fábio Gessner <fabiogessner@hotmail.com>
2025-04-28 22:50:47 -0300

    initial commit

--- 1_FindSum/src/main/java/org/example/Main.java
+++ 1_FindSum/src/main/java/org/example/Main.java
@@ -3,13 +3,14 @@ package org.example;
 import java.util.List;
 
 public class Main {
+
     /**
      * This problem was recently asked by Google.     *
      * Given a list of numbers and a number k, return whether any two numbers from the list add up to k.     *
      * For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
      */
     public static void main(String[] args) {
-        List<Integer> numbers = List.of(10, 15,6,3, 7);
+        List<Integer> numbers = List.of(10, 15, 6, 3, 7);
         int k = 13;
         System.out.println("Result: " + verifySum(numbers, k));
     }
